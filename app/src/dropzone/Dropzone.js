c04675dd3aee72758d3f8e66b6b4738e5f67e509
capntechbeard <capntechbeard@gmail.com>
2019-06-07 16:02:55 -0500

    Creating a Dropzone
    Continuation of the React File Upload tutorial that teaches you how to create the Dropzone component necessary to make the app work.
    
    https://malcoded.com/posts/react-dropzone/

---
+++ app/src/dropzone/Dropzone.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import "./Dropzone.css";
+
+class Dropzone extends Component {
+  constructor(props) {
+    super(props);
+    this.fileInputRef = React.createRef();
+
+    this.openFileDialog = this.openFileDialog.bind(this);
+    this.onFilesAdded = this.onFilesAdded.bind(this);
+  }
+
+  openFileDialog() {
+    if (this.props.disabled) return;
+    this.fileInputRef.current.click();
+  }
+
+  onFilesAdded(evt) {
+    if (this.props.disabled) return;
+    const files = evt.target.files;
+    if (this.props.onFilesAdded) {
+      const array = this.fileListToArray(files);
+      this.props.onFilesAdded(array);
+    }
+  }
+
+  fileListToArray(list) {
+    const array = [];
+    for (var i = 0; i < list.length; i++) {
+      array.push(list.item(i));
+    }
+    return array;
+  }
+
+  render() {
+    return (
+      <div
+        className="Dropzone"
+        onClick={this.openFileDialog}
+        style={{ cursor: this.props.disabled ? "default" : "pointer" }}
+      >
+        <img
+          alt="upload"
+          className="Icon"
+          src="baseline-cloud_upload-24px.svg"
+        />
+        <input
+          ref={this.fileInputRef}
+          className="FileInput"
+          type="file"
+          multiple
+          onChange={this.onFilesAdded}
+        />
+        <span>Upload Files</span>
+      </div>
+    );
+  }
+}
